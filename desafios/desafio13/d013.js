77a6a1c73dda7cf2e35b17b4186951c1fbf7d236
Nathalya <160874305+Nathalyagsn@users.noreply.github.com>
2024-05-03 15:38:52 -0300

    Desafio13

---
+++ desafios/desafio13/d013.js
@@ -0,0 +1,6 @@
+let pontos = prompt('Quantos pontos você fez? ');
+if (pontos >= 100){
+    console.log(`Parabéns, você fez ${pontos}! Ótima vitória!`)
+}else {
+    console.log(`Poxa, você fez apenas ${pontos}. Tente novamente para ganhar.`)
+}
