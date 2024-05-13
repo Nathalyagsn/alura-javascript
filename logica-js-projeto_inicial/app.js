4b90942ecfc811eaca6b70f2af011ee0cabbf7ec
Nathalya <160874305+Nathalyagsn@users.noreply.github.com>
2024-05-13 10:32:00 -0300

    update

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -4,18 +4,22 @@ let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual !=
-while (chute!= numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
-    if (chute == numeroSecreto){
-      alert(`Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-  } else {
-      if (numeroSecreto > chute){
-        alert(`O número secreto é maior que ${chute}.`);
-      } else {
-        alert(`O numero secreto é menor que ${chute}.`);
-      }
-      //tentativas = tentativas + 1; faz a mesma coisa que o de baixo mas é mais longo
-      tentativas++;
+  while (chute!= numeroSecreto) {
+      chute = prompt('Escolha um número entre 1 e 10');
+      if (chute == numeroSecreto){
+        break;
+    } else {
+        if (numeroSecreto > chute){
+          alert(`O número secreto é maior que ${chute}.`);
+        } else {
+          alert(`O numero secreto é menor que ${chute}.`);
+        }
+        //tentativas = tentativas + 1; faz a mesma coisa que o de baixo mas é mais longo
+        tentativas++;
+    }
   }
+  if(tentativas > 1){
+alert(`Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+} else {
+  alert(`Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
 }
- 
